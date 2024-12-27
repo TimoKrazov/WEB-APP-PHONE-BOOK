40f2e4e9271b9759abc43f3a0090315c6eff02fc
Timo Krazov <83912804+TimoKrazov@users.noreply.github.com>
2024-12-28 00:56:49 +0800

    Delete timo-krazov.ru/www directory

--- timo-krazov.ru/www/exitsess.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start(); 
-    // разрегистрировали переменные 
-    unset($_SESSION['idsess']);  
-    unset($_SESSION['name']); 
-    unset($_SESSION['secname']); 
-    unset($_SESSION['hashpasswd']); 
-    header('Location: /index.php'); // Пересылка на главную страницу 
-?>
